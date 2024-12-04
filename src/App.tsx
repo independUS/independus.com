828fecc4b24ffdee2774bba14a88a7a1922aaa18
Blaise <admin@independus.com>
2024-12-03 18:42:26 -0600

    test

--- src/App.tsx
+++ src/App.tsx
@@ -27,11 +27,9 @@ function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        🥳
         <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
+        
       </div>
     </main>
   );
